4a91093c976752d5ae6a16662c61948baa73b07b
Jan Vlk <jan.vlk@mensa.cz>
2024-10-09 18:20:05 +0200

    add directory support

--- vis_pcd.py
+++ vis_pcd.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 import argparse
+from pathlib import Path
 
 import numpy as np
 import open3d as o3d
@@ -25,9 +26,6 @@ def parse_args() -> argparse.Namespace:
         help="Name of the dataset, default: scala3",
     )
     parser.add_argument(
-        "-s", "--save_dir", type=str, help="Path to save the nonground points"
-    )
-    parser.add_argument(
         "-p", "--processed", action="store_true", help="Data file is processed"
     )
     parser.add_argument(
@@ -37,34 +35,55 @@ def parse_args() -> argparse.Namespace:
     return parser.parse_args()
 
 
-def load_pcd(
-    pcd_path: str, dataset: str, idx: int, processed: bool = False
-) -> np.ndarray:
-    if dataset == "scala3":
-        if processed:
-            pcd = np.load(pcd_path, allow_pickle=True)["raw_points"][idx]
+class Visualizer:
+    def __init__(self, args):
+        self.args = args
+
+    def load_pcd(self, pcd_path: str) -> np.ndarray:
+        if self.args.dataset == "scala3":
+            if self.args.processed:
+                pcd = np.load(pcd_path, allow_pickle=True)["raw_points"][
+                    self.args.index
+                ]
+            else:
+                pcd = np.load(pcd_path, allow_pickle=True)["arr_0"].item()["pc"][:, :3]
+        elif self.args.dataset == "pone":
+            if self.args.processed:
+                raise NotImplementedError("Processed PONE dataset is not supported yet")
+            else:
+                scan = np.load(pcd_path, allow_pickle=True)["scan_list"][
+                    self.args.index
+                ]
+                pcd = np.concatenate([scan["x"], scan["z"].reshape(-1, 1)], axis=1)
+        elif self.args.dataset == "rm_ground":
+            pcd = np.loadtxt(pcd_path)
         else:
-            pcd = np.load(pcd_path, allow_pickle=True)["arr_0"].item()["pc"][:, :3]
-    elif dataset == "pone":
-        if processed:
-            raise NotImplementedError("Processed PONE dataset is not supported yet")
+            raise ValueError(f"Unsupported dataset: {self.args.dataset}")
+        return pcd
+
+    def visualize(self, xyz: np.ndarray) -> None:
+        pcd = o3d.geometry.PointCloud()
+        pcd.points = o3d.utility.Vector3dVector(xyz)
+
+        o3d.visualization.draw_geometries([pcd])
+
+    def run(self):
+        if Path(self.args.pcd_path).is_dir():
+            files = sorted(Path(self.args.pcd_path).glob("*.*"))
+            if self.args.index is not None:
+                xyz = self.load_pcd(files[self.args.index])
+                self.visualize(xyz)
+            else:
+                for file in files:
+                    xyz = self.load_pcd(file)
+                    self.visualize(xyz)
         else:
-            scan = np.load(pcd_path, allow_pickle=True)["scan_list"][idx]
-            pcd = np.concatenate([scan["x"], scan["z"].reshape(-1, 1)], axis=1)
-    else:
-        raise ValueError(f"Unsupported dataset: {dataset}")
-    return pcd
-
-
-def visualize(xyz: np.ndarray) -> None:
-    pcd = o3d.geometry.PointCloud()
-    pcd.points = o3d.utility.Vector3dVector(xyz)
-
-    o3d.visualization.draw_geometries([pcd])
+            xyz = self.load_pcd(self.args.pcd_path)
+            self.visualize(xyz)
 
 
 if __name__ == "__main__":
     args = parse_args()
 
-    xyz = load_pcd(args.pcd_path, args.dataset, args.index, args.processed)
-    visualize(xyz)
+    vis = Visualizer(args)
+    vis.run()
